1ee010dfa0a6e4618cc904f881f859c34d7112d2
Sam McCollum <smccollum@mccollum.enterprises>
2018-09-18 16:28:15 -0700

    Config resolution should happen fewer times

--- impl/src/main/java/es/eisig/mp_conf_files/FileInterpolator.java
+++ impl/src/main/java/es/eisig/mp_conf_files/FileInterpolator.java
@@ -10,13 +10,16 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+
 /**
  * @author kg6zvp
  */
 public class FileInterpolator {
 	public static URL interpolate(URL reference) {
 		try {
-			return interpolateURL(reference).toURI().toURL();
+			return interpolateURL(ConfigProvider.getConfig(), reference).toURI().toURL();
 		} catch(IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -24,13 +27,13 @@ public class FileInterpolator {
 
 	public static File interpolate(File input) {
 		try {
-			return interpolateURL(input.toURI().toURL());
+			return interpolateURL(ConfigProvider.getConfig(), input.toURI().toURL());
 		} catch(IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	private static File interpolateURL(URL reference) throws IOException {
+	private static File interpolateURL(Config config, URL reference) throws IOException {
 		URLConnection conn = reference.openConnection();
 		conn.setUseCaches(false);
 		File tFile = File.createTempFile(reference.getFile(), "interpolated");
@@ -46,7 +49,7 @@ public class FileInterpolator {
 
 			String currentLine = null;
 			while((currentLine = bufferedReader.readLine()) != null) {
-				bufferedWriter.append(StringInterpolator.interpolate(currentLine) + "\n");
+				bufferedWriter.append(StringInterpolator.interpolate(config, currentLine) + "\n");
 			}
 		} catch (IOException e) {
 		} finally {
